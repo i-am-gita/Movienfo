2bd6f018d5cd35caa4886ca5ad640488d9a051fc
gita97 <etr.aleksandar.gitaric@gmail.com>
2020-04-04 17:14:59 +0200

    Updated .gitignore

--- app/src/main/java/pmf/android/movienfo/HomeActivity.java
+++ app/src/main/java/pmf/android/movienfo/HomeActivity.java
@@ -2,9 +2,13 @@ package pmf.android.movienfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = HomeActivity.class.getSimpleName();
+    String apiKey = BuildConfig.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
